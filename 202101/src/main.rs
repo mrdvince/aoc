7600b6bd64f3a77d9b0936adb5399e247029429b
Vince <vincivenv@gmail.com>
2023-01-14 02:53:34 +0100

    aoc 2021 part two puzzle

--- 202101/src/main.rs
+++ 202101/src/main.rs
@@ -7,9 +7,33 @@ fn main() {
         return;
     }
     let file_path = &args[1];
-
     let file_contents = fs::read_to_string(file_path).expect("Failed to open data file");
 
+    let part_one_ans = part_one(&file_contents);
+    let part_two_ans = part_two(&file_contents);
+
+    println!("Ans part one {}", part_one_ans);
+    println!("Ans part two {}", part_two_ans);
+}
+fn part_two(file_contents: &String) -> i32 {
+    let lines: Vec<_> = file_contents
+        .lines()
+        .skip(1)
+        .filter_map(|line| line.parse::<i32>().ok())
+        .collect();
+    let mut count = 0;
+    let mut prev_sum = 0;
+    for window in lines.windows(3) {
+        let sum = window.iter().sum();
+        if sum > prev_sum {
+            count += 1;
+        }
+        prev_sum = sum;
+    }
+    count
+}
+
+fn part_one(file_contents: &String) -> i32 {
     let counter = file_contents
         .lines()
         .skip(1)
@@ -22,5 +46,5 @@ fn main() {
             }
         })
         .0;
-    println!("Ans {}", counter);
+    counter
 }
