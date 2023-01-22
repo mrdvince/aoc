e29a2a08cd91378e8653203b2ddb62d017d4c867
Vince <vincivenv@gmail.com>
2023-01-22 18:05:10 +0100

    part two

--- 202102/src/main.rs
+++ 202102/src/main.rs
@@ -12,6 +12,35 @@ fn main() {
     let mut file_contents = String::new();
     file.read_to_string(&mut file_contents)
         .expect("Failed to read file");
+    let (horizontal, depth, res) = part_one(&file_contents);
+    println!("Part One answer: {}, {}, {}", horizontal, depth, res);
+
+    let (horizontal, depth, res) = part_two(&file_contents);
+    println!("Part Two answer: {}, {}, {}", horizontal, depth, res)
+}
+fn part_two(file_contents: &String) -> (i32, i32, i32) {
+    let (mut horizontal, mut depth, mut aim) = (0, 0, 0);
+    for line in file_contents.lines() {
+        let direction_depth: Vec<&str> = line.split(" ").filter(|s| !s.is_empty()).collect();
+        if direction_depth.len() < 2 {
+            continue;
+        }
+        match direction_depth[0] {
+            "forward" => {
+                let forward_value = direction_depth[1].parse().unwrap_or(0);
+                horizontal += forward_value;
+                depth += aim * forward_value;
+            }
+            "down" => aim += direction_depth[1].parse().unwrap_or(0),
+            "up" => aim -= direction_depth[1].parse().unwrap_or(0),
+            _ => continue,
+        }
+    }
+    let res = horizontal * depth;
+    (horizontal, depth, res)
+}
+
+fn part_one(file_contents: &String) -> (i32, i32, i32) {
     let (mut horizontal, mut depth) = (0, 0);
     for line in file_contents.lines() {
         let direction_depth: Vec<&str> = line.split(" ").filter(|s| !s.is_empty()).collect();
@@ -26,5 +55,5 @@ fn main() {
         }
     }
     let res = horizontal * depth;
-    println!("{}, {}, {}", horizontal, depth, res)
+    (horizontal, depth, res)
 }
